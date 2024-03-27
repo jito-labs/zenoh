ea9355c4a749ec8cd37e76f766f7de7b64e7808b
OlivierHecart <olivier.hecart@adlinktech.com>
2024-03-27 11:46:44 +0100

    Don't apply open session delay in client mode

--- zenoh/src/session.rs
+++ zenoh/src/session.rs
@@ -56,6 +56,7 @@ use uhlc::HLC;
 use zenoh_buffers::ZBuf;
 use zenoh_collections::SingleOrVec;
 use zenoh_config::unwrap_or_default;
+use zenoh_config::WhatAmI;
 use zenoh_core::{zconfigurable, zread, Resolve, ResolveClosure, ResolveFuture, SyncResolve};
 use zenoh_protocol::core::EntityId;
 use zenoh_protocol::network;
@@ -865,8 +866,10 @@ impl Session {
             .await;
             session.owns_runtime = true;
             runtime.start().await?;
-            // Workaround for the declare_and_shoot problem
-            tokio::time::sleep(Duration::from_millis(*API_OPEN_SESSION_DELAY)).await;
+            if runtime.whatami() != WhatAmI::Client {
+                // Workaround for the declare_and_shoot problem
+                tokio::time::sleep(Duration::from_millis(*API_OPEN_SESSION_DELAY)).await;
+            }
             Ok(session)
         })
     }
